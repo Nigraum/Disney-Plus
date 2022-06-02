7c1f9d8bee2161f752dcef0c9b16acf14fac829b
Nigraum <yan.corintima@hotmail.com>
2022-06-02 13:16:56 -0300

    Fixing js and some changes on index.html

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,34 +1,41 @@
 const API_KEY = 'f3548c64fd91ca79a83e0fd855d2ca79'
-const LIST_MOVIES = ['tt5613484', 'tt1032755', 'tt4846232', 'tt0113540', 'tt0355702', 'tt0117951' ]
-const BUTTON_PLAY = '<button type="button"><img src="../assets/icon-play-button.png" alt="Icon play button"></button>'
+const BASE_URL_IMAGE = 'https://image.tmdb.org/t/p/original'
+const LIST_MOVIES = ['tt5613484', 'tt4846232', 'tt1032755', 'tt0113540', 'tt0355702', 'tt0117951', 'tt5727208', 'tt2210479', 'tt2358913', 'tt2358911']
+
+const moviesList = document.getElementById('movies__list')
 
 function getUrlMovie(movieId) {
-    return `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`
+  return `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`
 }
 
-fetch(getUrlMovie(LIST_MOVIES[0])).then(response => response.json()).then(data => {
-        console.log(data)
-        const app = document.getElementById('app')
+function setMainMovie(movieId) {
+  fetch(getUrlMovie(movieId)).then(response => response.json()).then(data => {
+    const app = document.getElementById('app')
+  
+    const title = document.querySelector('.movie h1')
+    const description = document.querySelector('.movie p')
+    const info = document.querySelector('.movie span')
+    const rate = document.querySelector('.rate strong')
+  
+    const yearRelease = data.release_date.split('-')[0]
+  
+    title.innerHTML = data.title
+    description.innerHTML = data.overview
+    rate.innerHTML = data.vote_average
+    info.innerHTML = yearRelease + ' - ' + data.genres[0].name + ' - Movie'
+  
+    const image = BASE_URL_IMAGE.concat(data.backdrop_path)
+    app.style.backgroundImage = `linear-gradient(90.18deg, rgba(13, 22, 46, 0.7) 23.21%, rgba(13, 22, 46, 0.0001) 96.69%), url('${image}')`
+  })
+}
 
-        const title = document.querySelector('.movie h1')
-        const description = document.querySelector('.movie p')
-        const rate = document.querySelector('.rate strong')
-        const info = document.querySelector('.movie span')
+function createButtonMovie(movieId) {
+  const button = document.createElement('button')
+  button.setAttribute('onclick', `setMainMovie('${movieId}')`)
+  button.innerHTML = '<img src="/assets/icon-play-button.png" alt="Icon play button" />'
 
-        const yearRelease = data.release_date.split('-')[0]
-
-        title.innerHTML = data.title
-        description.innerHTML = data.overview
-        rate.innerHTML = data.vote_average
-        info.innerHTML = yearRelease + ' - ' + data.genres[0].name + ' - Movie '
-
-        const image = `https://image.tmdb.org/t/p/original${data.backdrop_path}`
-        app.style.backgroundImage = `linear-gradient(90.18deg, rgba(13, 22, 46, 0.7) 23.21%, rgba(13, 22, 46, 0.0001) 96.69%), url('${image}')`
-
-    })
-
-
-const moviesList = document.getElementById('movies_list')
+  return button
+}
 
 function createMovie(movieId) {
   fetch(getUrlMovie(movieId)).then(response => response.json()).then(data => {
@@ -50,4 +57,4 @@ function loadListMovies() {
 
 loadListMovies()
 
-
+setMainMovie(LIST_MOVIES[0])
